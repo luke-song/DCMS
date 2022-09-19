435a9da71d3261141e3eac67a93b54c095e9efc4
Minjae Kwon <minjaekwon9@gmail.com>
2022-09-18 21:04:51 -0700

    Merged code.

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-import { Box, Heading, Container, VStack, Stack } from "@chakra-ui/react";
-import Form from "../Form/EmailForm";
-=======
-import { Box, Heading, Container, VStack, Stack, Flex,Spacer } from "@chakra-ui/react";
+import { Box, Heading, Container, VStack, Stack, Flex, Spacer } from "@chakra-ui/react";
 import Form from "../Form/email_form";
->>>>>>> 99b6092ca158989447cc79b4ce0fda5f561ad1b3
 
 export default function Contact() {
 	return (
 		<Box>
 			<Container mt={50} maxW="90%" display="flex">
-            <Flex direction="row" w={"95%"} >
+				<Flex direction="row" w={"95%"} >
 					<VStack align="flex-start">
 						<Heading color="#FF6467" size="2xl">
 							Contact us.
@@ -23,18 +18,14 @@ export default function Contact() {
 							We will get back to you as soon as possible.
 						</Heading>
 					</VStack>
-<<<<<<< HEAD
-					<VStack w={650} bg="#464549" borderRadius={20} p={20}>
-=======
-                    <Spacer/>
-                    <VStack w={{ md: '55%' }} justify="center" spacing={3} bg="#464549" borderRadius={20} p={20}>
->>>>>>> 99b6092ca158989447cc79b4ce0fda5f561ad1b3
+					<Spacer />
+					<VStack w={{ md: '55%' }} justify="center" spacing={3} bg="#464549" borderRadius={20} p={20}>
 						<Heading color="#FF6467" size="md">
 							What's your question?
 						</Heading>
 						<Form />
 					</VStack>
-                </Flex>
+				</Flex>
 			</Container>
 		</Box>
 	);
--- src/components/Upload/upload.jsx
+++ src/components/Upload/upload.jsx
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-import { Box, Heading, Container, VStack, Stack } from "@chakra-ui/react";
-import Form from "../Form/Form";
-=======
 import { Box, Heading, Container, VStack, Stack, Flex, Spacer } from "@chakra-ui/react";
 import Form from "../Form/form";
->>>>>>> 99b6092ca158989447cc79b4ce0fda5f561ad1b3
 
 export default function Upload() {
 	return (
 		<Box>
 			<Container mt={50} maxW="90%" display="flex">
-            <Flex direction="row" w={"95%"} >
+				<Flex direction="row" w={"95%"} >
 					<VStack align="flex-start">
 						<Heading color="#FF6467" size="2xl">
 							Welcome, User
@@ -28,16 +23,16 @@ export default function Upload() {
 							</span>
 						</Heading>
 					</VStack>
-                    
-                    <Spacer/>
-                    
+
+					<Spacer />
+
 					<VStack w={{ md: '55%' }} justify="center" spacing={3} bg="#464549" borderRadius={20} p={20}>
 						<Heading color="#FF6467" size="md">
 							Upload Your Content
 						</Heading>
-                        <Box alignItems={"center"}>
-						<Form />
-                        </Box>
+						<Box alignItems={"center"}>
+							<Form />
+						</Box>
 					</VStack>
 				</Flex>
 			</Container>
